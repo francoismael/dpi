14a5f9cb69ffe4fea272c3f50dbc6ef71d39836a
RaselisonToky <toky.raselison@ic.mg>
2025-04-16 16:19:24 +0300

    [AppModule]add PatientModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientModule } from './modules/patient/patient.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PatientModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
